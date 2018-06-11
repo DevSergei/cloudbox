2a7737355935717b33c23984804c07de33f904b3
DevSergei <bakaikin@mail.ru>
2018-06-12 00:20:49 +0300

    Initial commit

---
+++ client/src/main/java/Controller.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.scene.input.DragEvent;
+
+public class Controller {
+    public void sendFile(DragEvent dragEvent) {
+    }
+
+    public void draggingOver(DragEvent dragEvent) {
+    }
+
+    public void downloadFile(ActionEvent actionEvent) {
+    }
+
+    public void deleteFile(ActionEvent actionEvent) {
+    }
+
+    public void updateFiles(ActionEvent actionEvent) {
+    }
+}
